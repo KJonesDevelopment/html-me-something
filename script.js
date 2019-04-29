c3d83ffa0737634f7116b9d726a3f833dc692682
kdjones728 <kd.jones728@gmail.com>
2019-04-29 13:56:42 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function contactInfo(){
+    document.getElementById("teaInput").innerHTML="Thank you!";
+        
+}
+
+function normalize(){
+    document.getElementById("introButton").innerHTML = 
+        "KATHRYN JONES";
+}
